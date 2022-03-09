1ad711401b59772b83490f6b1df97862e4d7fecc
sarahma123 <94800595+sarahma123@users.noreply.github.com>
2022-03-08 23:16:25 -0800

    Loader io verification added

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ app.get('/products/:id/styles', models.getStyles);
 
 app.get('/products/:id/related', models.getRelated);
 
+app.get('/loaderio-01ee2201a1d45c621f7cb1f7d49bfde7.txt', (req, res) => {
+  res.status(200).send('loaderio-01ee2201a1d45c621f7cb1f7d49bfde7');
+})
+
 app.listen(3000, () => {
   console.log('listening on porrrt');
 })
