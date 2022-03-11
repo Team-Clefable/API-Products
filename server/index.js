08e1f75c42d112c47ade2b4cb8b6216161b49007
sarahma123 <94800595+sarahma123@users.noreply.github.com>
2022-03-10 22:30:27 -0800

    .env added, loader io string updated

--- server/index.js
+++ server/index.js
@@ -10,8 +10,8 @@ app.get('/products/:id/styles', models.getStyles);
 
 app.get('/products/:id/related', models.getRelated);
 
-app.get('/loaderio-cdb9dd42004cce02c46b09e4705b2e1a.txt', (req, res) => {
-  res.status(200).send('loaderio-cdb9dd42004cce02c46b09e4705b2e1a');
+app.get('/loaderio-3edc732ab67d8563f4d679851c68e65e.txt', (req, res) => {
+  res.status(200).send('loaderio-3edc732ab67d8563f4d679851c68e65e');
 })
 
 app.listen(3000, () => {
